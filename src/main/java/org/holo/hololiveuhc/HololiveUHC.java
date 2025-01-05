026ed559140bbede52656044e3357f7b1991afda
Lucien Verhaege <lucienvrhg@gmail.com>
2025-01-05 15:32:38 +0100

    initial commit

---
+++ src/main/java/org/holo/hololiveuhc/HololiveUHC.java
@@ -0,0 +1,17 @@
+package org.holo.hololiveuhc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HololiveUHC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
